09b026c8aacc446e92b47e67ab3de17ea27291ad
pilentboy <sirxmahdi79@gmail.com>
2023-10-01 19:46:28 +0330

    add custom colors and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary":"#171420",
+        "secondary":"#1f1d2c",
+        "light-purple":"#9694e7",
+        "purple":"#5f65f4",
+        "blue":"#2a4af4",
+        "orange":"#df8600"
+      }
+    },
   },
   plugins: [],
 }
