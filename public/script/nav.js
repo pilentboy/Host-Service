12c078f3291fd73016d93ecf6043d22286b310f8
pilentboy <sirxmahdi79@gmail.com>
2023-10-03 00:27:58 +0330

    fix script and css src

--- public/script/nav.js
+++ public/script/nav.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
+     console.log("test")
     $(window).scroll(function () { 
      // add border bottom to nav when scoll top is over 28 
        if( $(window).scrollTop() > 28){
             $("#nav").addClass("border-b-2");
             $("#nav").addClass("border-gray-500");
-
        }else{
              // remove border bottom to nav when scoll top is less than 29 
             $("#nav").removeClass("border-b-2");
