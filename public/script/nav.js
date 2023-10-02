9b07ac739b2e2ce3daaf93bf367439cd84fab850
pilentboy <sirxmahdi79@gmail.com>
2023-10-03 00:46:26 +0330

    add bg-gray for nav scroll

--- public/script/nav.js
+++ public/script/nav.js
@@ -5,10 +5,15 @@ $(document).ready(function () {
        if( $(window).scrollTop() > 28){
             $("#nav").addClass("border-b-2");
             $("#nav").addClass("border-gray-500");
+            $("#nav").addClass("bg-gray-800");
+            $("#nav").addClass("opacity-80");
+
        }else{
              // remove border bottom to nav when scoll top is less than 29 
             $("#nav").removeClass("border-b-2");
             $("#nav").removeClass("border-gray-500");
+            $("#nav").removeClass("bg-gray-800");
+            $("#nav").removeClass("opacity-80");
        }
        
     });
